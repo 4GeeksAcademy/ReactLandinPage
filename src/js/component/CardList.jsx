ca34e60badf1a22431f1d1cb3adf6279bb7d5507
agumanz <128185771+agumanz@users.noreply.github.com>
2023-06-07 16:51:29 +0000

    ejercisioReact

---
+++ src/js/component/CardList.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const CardList = () => {
+    return <div className="container">
+        <div class="card-group">
+    <div class="card">
+      <img src="https://www.publicdomainpictures.net/pictures/280000/nahled/dusky-grey-sky-background.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">
+        <button className="btn btn-primary btn-sm d-grid gap-2 col-6 mx-auto" type="button">Find out more!</button>
+        </small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://www.publicdomainpictures.net/pictures/280000/nahled/dusky-grey-sky-background.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">
+        <button className="btn btn-primary btn-sm d-grid gap-2 col-6 mx-auto" type="button">Find out more!</button>
+        </small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://www.publicdomainpictures.net/pictures/280000/nahled/dusky-grey-sky-background.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">
+        <button className="btn btn-primary btn-sm d-grid gap-2 col-6 mx-auto" type="button">Find out more!</button>
+        </small>
+        </div>
+      </div>
+      <div class="card">
+      <img src="https://www.publicdomainpictures.net/pictures/280000/nahled/dusky-grey-sky-background.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">
+        <button className="btn btn-primary btn-sm d-grid gap-2 col-6 mx-auto" type="button">Find out more!</button>
+        </small>
+      </div>
+    </div>
+  </div>
+  </div>
+}
---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Footer = () => {
+    return <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '20px' }}>
+    <p>2023 ©  Mi página web. Todos los derechos reservados.</p>
+  </footer>
+}
